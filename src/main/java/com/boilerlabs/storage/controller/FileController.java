4a60f6f4337d109dcc73f4e2d0353ccbe10cbe2e
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-10-02 23:46:35 -0300

    feat: add FileController

---
+++ src/main/java/com/boilerlabs/storage/controller/FileController.java
@@ -0,0 +1,26 @@
+package com.boilerlabs.storage.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.boilerlabs.storage.service.FileStorageService;
+
+@RestController
+@RequestMapping("/api/files")
+public class FileController {
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(
+            @RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok(fileStorageService.storeFile(file));
+    }
+
+}
